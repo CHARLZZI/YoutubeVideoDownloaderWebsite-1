699a5da1b325b3694067339b327c0fd3944ebd14
SingleSimha <70630469+SingleSimha@users.noreply.github.com>
2022-03-03 13:40:37 +0530

    added yt video downloader
    Just added the basic one which is simple... will add quality features soon

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
 const express = require('express')
+const {chain,last} = require('lodash')
+const ytdl = require('ytdl-core')
+const {validate, Joi} = require('express-validation')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 app.use(express.static('public'))
 
-app.post('/ytUrl',(req,res)=>{
+const getResolutions = (formats) => {
 
-    console.log(req.body.url)
-    res.redirect(req.body.url)
+    return chain(formats)
+    .filter(format=>{return format.height})
+    .map(format=>{return format.height})
+    .uniq()
+    .orderBy(null,'desc')
+    .value()
+}
+
+app.get('/ytUrl',(req,res,next)=>{
+
+    const {id,url} = req.query
+
+    res.header('Content-Disposition','attachment; filename="video.mp4"')
+
+    let a = ytdl(url,{
+        format:'mp4'
+    }).pipe(res)
+    
+
+    // ytdl.getInfo(id).then(({videoDetails,formats})=>{
+
+    //     const {title, thumbnails} = videoDetails
+
+    //     const thumbnailURL = last(thumbnails).url
+
+    //     const resolutions = getResolutions(formats)
+
+    //     console.log(formats)
+    //     console.log(typeof formats)
+
+    //     res.json({title,thumbnailURL,resolutions})
+
+    // }).catch(err=>{next(err)})
 
 })
 
--- public/YtD.js
+++ public/YtD.js
@@ -1,11 +1,9 @@
 async function getVideoInfo(){
 
-    console.log('started')
-    let videoId = '7JmcmaAXzks'
-    let eurl = `https://youtube.googleapis.com/v3/${videoId}`
-    const response = await axios.get(`https://www.youtube.com/get_video_info?video_id=${videoId}&eurl=https%3A%2F%2Fyoutube.googleapis.com%2Fv%2F${videoId}&html5=1&c=TVHTML5&cver=6.20180913`);
-    // const response = await axios.get(`${eurl}`);
-    const parsedResponse = Object.fromEntries(new URLSearchParams(response.data));
-    console.log(parsedResponse)
+    let url = document.getElementById('ytUrl').value
+    let id = "LoaG5G5L8x0"
+    // let data =  await axios.get(`/ytUrl?id=${id}&&url=${"https://www.youtube.com/watch?v=gpLJKNPtKB8"}`)
+    location.href = `/ytUrl?id=${id}&&url=${url}`
+    console.log(data)
 
 }
