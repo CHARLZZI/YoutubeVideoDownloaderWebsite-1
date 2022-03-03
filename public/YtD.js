55c727b5f9ab9435697869764ac3f7bf4a64b07a
SingleSimha <70630469+SingleSimha@users.noreply.github.com>
2022-03-03 11:15:32 +0530

    added gitignore file

--- public/YtD.js
+++ public/YtD.js
@@ -1,15 +1,11 @@
-let submitBtn = document.getElementById('submit-btn')
-console.log('value :',submitBtn.value)
-// let ytUrl = document.getElementById('ytUrl')
-// submitBtn.addEventListener("click",async e=>{
-//     e.preventDefault()
-//     console.log('lol')
-//     await axios({
-//         method: 'post',
-//         url: '/ytUrl',
-//         headers: {}, 
-//         data: {
-//           url: ytUrl,
-//         }
-//       });
-// })
+async function getVideoInfo(){
+
+    console.log('started')
+    let videoId = '7JmcmaAXzks'
+    let eurl = `https://youtube.googleapis.com/v3/${videoId}`
+    const response = await axios.get(`https://www.youtube.com/get_video_info?video_id=${videoId}&eurl=https%3A%2F%2Fyoutube.googleapis.com%2Fv%2F${videoId}&html5=1&c=TVHTML5&cver=6.20180913`);
+    // const response = await axios.get(`${eurl}`);
+    const parsedResponse = Object.fromEntries(new URLSearchParams(response.data));
+    console.log(parsedResponse)
+
+}
